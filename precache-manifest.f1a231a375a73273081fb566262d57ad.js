ad26354fb9067c703f4e8821ab3faf5a1a516ca3
Armando Hernandez de la Vega <0219796@gmail.up.edu.mx>
2020-08-05 23:14:10 -0500

    Updates

---
+++ precache-manifest.f1a231a375a73273081fb566262d57ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a310968bc9422d458982e511bca7c343",
+    "url": "/wecan/index.html"
+  },
+  {
+    "revision": "200bf9bc33c52bb77eb5",
+    "url": "/wecan/static/css/main.cd75127c.chunk.css"
+  },
+  {
+    "revision": "d1702fea9a442a82be21",
+    "url": "/wecan/static/js/2.122165b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wecan/static/js/2.122165b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200bf9bc33c52bb77eb5",
+    "url": "/wecan/static/js/main.627ef3cf.chunk.js"
+  },
+  {
+    "revision": "027fc7936b2cf95f62e9",
+    "url": "/wecan/static/js/runtime-main.2757cdf9.js"
+  },
+  {
+    "revision": "3a451edeb995e40bd3614bdecfe0ab9a",
+    "url": "/wecan/static/media/actIcon.3a451ede.png"
+  },
+  {
+    "revision": "41f1530e3b7e36bab47a1453b2eb47c2",
+    "url": "/wecan/static/media/activitiesWeCan.41f1530e.png"
+  },
+  {
+    "revision": "b4139630fd7e53f959f51aa23c142395",
+    "url": "/wecan/static/media/educationWeCan.b4139630.png"
+  },
+  {
+    "revision": "d8d9271899df30f63375624f6e942135",
+    "url": "/wecan/static/media/psychologists.d8d92718.png"
+  },
+  {
+    "revision": "33e15b12fbd23b8d1210e7c4d5952b3b",
+    "url": "/wecan/static/media/therapyIcon.33e15b12.png"
+  },
+  {
+    "revision": "29bb77e6e9eed7c64f23c103f78e031a",
+    "url": "/wecan/static/media/weCanIntroImg.29bb77e6.png"
+  }
+]);
